1d70dd3a9360c78286b8a876e4d4c07fab4fa671
Shoganai <business.souhail@gmail.com>
2021-09-21 19:03:53 +0100

    Replaced onComplete with gsap>timeline

--- app.js
+++ app.js
@@ -3,30 +3,36 @@ const nav=document.querySelectorAll('.nav-item');
 const hero_element=document.querySelectorAll('.header-element');
 
 
+const TL = gsap.timeline();
 
-gsap.from(nav,{
-    autoAlpha:0,
-    y:-100,
-    duration:1,
-    stagger:0.3,
-    onComplete:()=>{
-        gsap.from(card,{
-            autoAlpha:0,
-            //y:-200,
-            x:-300,
-            rotation:-30,
-            duration:1,
-            ease:'power4.out',
-            onComplete:()=>{
+TL.from(nav,{autoAlpha:0,y:-100,duration:1,stagger:0.3})
+.from(hero_element,{autoAlpha:0,y:-300,duration:2,stagger:.2},'-=.3')
+.from(card,{autoAlpha:0, x:-300, rotation:-30, duration:1,ease:'power4.out',},'-=.3')
+
+
+// gsap.from(nav,{
+//     autoAlpha:0,
+//     y:-100,
+//     duration:1,
+//     stagger:0.3,
+//     onComplete:()=>{
+//         gsap.from(card,{
+//             autoAlpha:0,
+//             //y:-200,
+            // x:-300,
+            // rotation:-30,
+            // duration:1,
+            // ease:'power4.out',
+//             onComplete:()=>{
                 
-            gsap.from(hero_element,{
-                autoAlpha:0,
-                y:-300,
-                duration:2,
-                stagger:.2,
-            })
-         }
+//             gsap.from(hero_element,{
+//                 autoAlpha:0,
+//                 y:-300,
+//                 duration:2,
+//                 stagger:.2,
+//             })
+//          }
             
-        })
-    }
-})
+//         })
+//     }
+// })
